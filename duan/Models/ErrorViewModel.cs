119218c9be5c9c5b0cded4d85f1d99c6c5f85d94
Thanh10101 <trinhthanh20022@gmail.com>
2022-10-11 20:54:44 +0700

    first

---
+++ duan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace duan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
